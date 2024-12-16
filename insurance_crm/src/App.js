2bbae2073148286903dc154f77135be7a3eb9c70
Niranjan V. Kakatkar <57944396+niranjankakatkar@users.noreply.github.com>
2024-12-16 15:45:18 +0530

    16-12-2024

---
+++ insurance_crm/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Login from './pages/login'
+import Home from './pages/Home'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}></Route>
+      <Route path='/Dashboard' element={<Home/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
